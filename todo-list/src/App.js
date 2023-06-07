a1a37caa292b869fe93426e240a88ffea1b091cb
김밍슈 <kmsu2046@gmail.com>
2023-06-07 21:11:13 +0900

    Todo-list

---
+++ todo-list/src/App.js
@@ -0,0 +1,12 @@
+import TodoInsert from "./components/TodoInsert";
+import TodoTemplate from "./components/TodoTemplate";
+
+const App = () => {
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+    </TodoTemplate>
+  );
+};
+
+export default App;
